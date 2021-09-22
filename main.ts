41db4a6b547777d2841c30cd8ab399b1093b3252
Kyoung Dal, Kim <64296073+JoshKim82@users.noreply.github.com>
2021-09-22 12:04:09 +0900

    쉬고 변수 추가

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 let 아침 = 1
 let 점심 = 2
 let 저녁 = 3
+let 쉬고 = 500
 basic.showNumber(아침)
-basic.pause(500)
+basic.pause(쉬고)
 basic.showNumber(점심)
-basic.pause(500)
+basic.pause(쉬고)
 basic.showNumber(저녁)
